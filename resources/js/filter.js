41ed2ad857743a9b93ddfed30b6a376bfc96dbd6
Zainde <aleaarup@gmail.com>
2018-08-14 13:14:57 +0200

    changes to js to work with eco

--- resources/js/filter.js
+++ resources/js/filter.js
@@ -6,24 +6,23 @@ var filterButton=Array.from(document.getElementsByClassName('filter'));
 window.onload=addclick
 // adds onclick function for all buttons
 function addclick(){
+  var categorybutton=Array.from(document.getElementsByClassName('catbut'));
 
 for (var i = 0; i < filterButton.length; i++) {
   filterButton[i].addEventListener('click',function () {
     // variable for filter category
   var filterCat=this.classList[1];
-
   // loop going through all sales articles
   for (var i = 0; i < SalesArt.length; i++) {
-
     // shows/hides so only the sales articles with the same class as the button pressed
+  if (SalesArt[i].classList.contains(filterCat)) {
+    categorybutton[i].classList.remove("hide");
+    categorybutton[i].classList.add("show");
 
-  if (SalesArt[i].classList[1]==filterCat) {
-    SalesArt[i].classList.remove("hide");
-    SalesArt[i].classList.add("show");
   }
-  else {
-  SalesArt[i].classList.remove("show");
-    SalesArt[i].classList.add("hide");
+  else{
+  categorybutton[i].classList.remove("show");
+    categorybutton[i].classList.add("hide");
   }
 }
 });
