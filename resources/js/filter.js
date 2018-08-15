2a6543039dde749f7211382f1765dd7c88426cf9
Zainde <aleaarup@gmail.com>
2018-08-15 10:14:15 +0200

    fold

--- resources/js/filter.js
+++ resources/js/filter.js
@@ -29,3 +29,23 @@ for (var i = 0; i < filterButton.length; i++) {
 });
 
 }}
+
+
+
+// Læs mere function
+window.onload=addfold
+
+// variable for filter buttons
+var readmore=Array.from(document.getElementsByClassName('foldout'));
+function addfold(){
+  for (var i = 0; i < readmore.length; i++) {
+    readmore[i].addEventListener('click', function(){
+      console.log(this);
+      for (var j = 0; j <  Array.from(document.getElementsByClassName("middle")).length; j++) {
+        if (  Array.from(document.getElementsByClassName("middle"))[j].classList.contains("animatefold")) {
+          Array.from(document.getElementsByClassName("middle"))[j].classList.remove("animatefold")
+        }      }
+        this.parentElement.classList.add("animatefold");
+    });
+  }
+}
